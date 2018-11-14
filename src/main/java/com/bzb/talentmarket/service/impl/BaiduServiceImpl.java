3819802cdfa6a9caf9158a99e11643f9afb3dc41
bzbstar <13140132880@163.com>
2018-11-14 21:30:46 +0800

    11

--- src/main/java/com/bzb/talentmarket/service/impl/BaiduServiceImpl.java
+++ src/main/java/com/bzb/talentmarket/service/impl/BaiduServiceImpl.java
@@ -1,16 +1,14 @@
 package com.bzb.talentmarket.service.impl;
 
-import com.bzb.talentmarket.service.BaiduService;
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import javax.jnlp.BasicService;
-import java.util.HashMap;
-import java.util.Map;
+import com.bzb.talentmarket.service.BaiduService;
 
 /**
  * @author bzb
